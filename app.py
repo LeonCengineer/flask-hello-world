73e606946d19f26dadb282aea762dc65292bd516
LeonCengineer <leonhcopeland@gmail.com>
2022-12-23 02:04:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+import tweepy
+
+auth = tweepy.OAuth1UserHandler(
+   hxlKCgcrMtFweITfuY2PVc0b6, YIoohvFDa1dsgWzl03pDtHyNXycmQZFTGDabiH9PHJTXu0sjDL, 18368941-6MRapL0PCELHw0RagwsyrgQntyoxUy91kjxBAHMQD, RkTOdUgFDvVzowFBgso65nmVoYPMASVFOb5PXCDZ278E
+)
+
+api = tweepy.API(auth)
+
+
+screen_name = "macroalf"
+
+statuses = api.user_timeline(screen_name)
+
+
+print(str(len(statuses)) + " number of statuses have been fetched.")
+
+
+
